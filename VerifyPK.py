33fab06dbb41df5be315bacd207cd6b61fd86343
E4crypt3d <gohramgkb@gmail.com>
2024-09-14 11:27:50 +0500

    improvements

--- VerifyPK.py
+++ VerifyPK.py
@@ -1,14 +1,11 @@
 import re
+from stringcolor import cs
 import requests
 from bs4 import BeautifulSoup
 from utils import get_headers, get_db
 from requests.exceptions import RequestException
 import argparse
 import sys
-from colorama import Fore, Style, init
-
-# Initialize colorama
-init(autoreset=True)
 
 
 class VerifyPK:
@@ -19,10 +16,10 @@ class VerifyPK:
     def is_number_valid(self):
         patterns = [r'^92\d{10}$', r'^03\d{9}$']
         if any(re.match(pattern, self.value) for pattern in patterns):
-            print(f"{Fore.GREEN}Phone number '{self.value}' is valid.")
+            print(cs(f"Phone number '{self.value}' is valid.", 'green'))
             return True
         print(
-            f"{Fore.RED}Invalid Number - Number must be in '92[0-9]' or '03[0-9]' format.\n")
+            cs("Invalid Number - Number must be in '92[0-9]' or '03[0-9]' format.\n", 'red'))
         return False
 
     def get_sim_database(self):
@@ -35,12 +32,11 @@ class VerifyPK:
                 response.raise_for_status()  # Raise an exception for HTTP errors
                 return response
             except RequestException as e:
-                print(f"{Fore.RED}{e}")
                 print(
-                    f"{Fore.YELLOW}Attempt {attempt + 1}: Connection Error. Retrying...\n")
+                    cs(f"Attempt {attempt + 1}: Connection Error. Retrying...\n", 'yellow'))
                 if attempt == 2:
                     print(
-                        f"{Fore.RED}Failed to connect after 3 attempts. Exiting...\n")
+                        cs("Failed to connect after 3 attempts. Try changing DNS to 1.1.1.1, use a VPN or check your internet connection.\n", 'red'))
                     sys.exit(1)
 
     def process_response(self, response):
@@ -54,15 +50,15 @@ class VerifyPK:
                 for i in range(0, len(sim_data_td), 2)
             }
             return sim_data
-        print(f"{Fore.RED}No Records Found for {self.value}\n")
+        print(cs(f"No Records Found for {self.value}\n", 'red'))
         return {}
 
     def validate_cnic(self):
         pattern = r"^\d{5}-?\d{7}-?\d{1}$"
         if re.match(pattern, self.value):
-            print(f"{Fore.GREEN}CNIC '{self.value}' is valid.")
+            print(cs(f"CNIC '{self.value}' is valid.", 'green'))
             return True
-        print(f"{Fore.RED}Invalid CNIC Number - Provide a valid CNIC Number.\n")
+        print(cs("Invalid CNIC Number - Provide a valid CNIC Number.\n", 'red'))
         return False
 
     def get_cnic_details(self):
@@ -75,12 +71,11 @@ class VerifyPK:
                 response.raise_for_status()
                 return response
             except RequestException as e:
-                print(f"{Fore.RED}{e}")
                 print(
-                    f"{Fore.YELLOW}Attempt {attempt + 1}: Connection Error. Retrying...\n")
+                    cs(f"Attempt {attempt + 1}: Connection Error. Retrying...\n", 'yellow'))
                 if attempt == 2:
                     print(
-                        f"{Fore.RED}Failed to connect after 3 attempts. Exiting...\n")
+                        cs("Failed to connect after 3 attempts. Try changing DNS to 1.1.1.1, use a VPN or check your internet connection.\n", 'red'))
                     sys.exit(1)
 
     def process_cnic_response(self, response):
@@ -97,39 +92,39 @@ class VerifyPK:
                 }
                 cnic_data_list.append(data_dict)
             return cnic_data_list
-        print(f"{Fore.RED}No Records Found for {self.value}\n")
+        print(cs(f"No Records Found for {self.value}\n", 'red'))
         return []
 
     def run(self):
-        print(f"{Fore.YELLOW}{Style.BRIGHT}\nCreated By E4CRYPT3D\n")
+        print(cs("\nCreated By E4CRYPT3D\n", "yellow").bold())
         try:
             if self.option == "-n" and self.is_number_valid():
                 response = self.get_sim_database()
                 data = self.process_response(response)
                 if data:
-                    print(f"{Fore.YELLOW}\nData Found for {self.value}:")
+                    print(cs(f"\nData Found for {self.value}:", 'yellow'))
                     self.print_data(data)
             elif self.option == "-c" and self.validate_cnic():
                 response = self.get_cnic_details()
                 data = self.process_cnic_response(response)
                 if data:
                     print(
-                        f"{Fore.YELLOW}\nTotal {len(data)} record(s) found for {self.value}:")
+                        cs(f"\nTotal {len(data)} record(s) found for {self.value}:", 'yellow'))
                     for record in data:
                         self.print_data(record)
             else:
-                print(f"{Fore.RED}Invalid option or value.")
+                print(cs("Invalid option or value.", 'red'))
                 sys.exit(1)
         except Exception as e:
-            print(f"{Fore.RED}An unexpected error occurred: {e}\n")
+            print(cs(f"An unexpected error occurred: {e}\n", 'red'))
             sys.exit(1)
 
     @staticmethod
     def print_data(data):
         if not data:
-            print(f"{Fore.RED}No data to display.")
+            print(cs("No data to display.", 'red'))
             return
-        print(f"{Fore.CYAN}\n Key        | Value      |")
+        print(cs("\n Key        | Value      |", 'cyan'))
         print("+-----------+------------+")
         for k, v in data.items():
             print(f" {k:<10} | {v:<10} |")
@@ -146,7 +141,7 @@ def main():
     args = parser.parse_args()
 
     if args.phone and args.cnic:
-        print(f"{Fore.RED}Error: You can specify only one option at a time. Use -n for phone number or -c for CNIC.\n")
+        print(cs("Error: You can specify only one option at a time. Use -n for phone number or -c for CNIC.\n", 'red'))
         sys.exit(1)
     elif args.phone:
         option = "-n"
@@ -155,8 +150,7 @@ def main():
         option = "-c"
         value = args.cnic
     else:
-        print(
-            f"{Fore.RED}Error: No valid arguments provided. Use -n <phone_number> or -c <cnic_id>.\n")
+        print(cs("Error: No valid arguments provided. Use -n <phone_number> or -c <cnic_id>.\n", 'red'))
         sys.exit(1)
 
     verifier = VerifyPK(option, value)
